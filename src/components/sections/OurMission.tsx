b885cc63411aa3a140d93ae05aee53491389b62a
Wiz Abhi <abhishekg8318@gmail.com>
2024-12-19 16:20:26 +0530

    added

--- src/components/sections/OurMission.tsx
+++ src/components/sections/OurMission.tsx
@@ -9,7 +9,7 @@ const OurMission = () => {
           <div className="order-2 md:order-1 text-white">
             <h2 className="text-4xl font-bold mb-6 text-cyan-400">Our Mission</h2>
             <p className="text-lg">
-              Our mission at 365 Business Origin is to provide exceptional products and services to our clients, 
+              Our mission at 365-Bizness is to provide exceptional products and services to our clients, 
               upholding the highest standards of quality and professionalism. We are dedicated to fostering enduring 
               partnerships with our clients, collaborating to achieve shared objectives and success.
             </p>
